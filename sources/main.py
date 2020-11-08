b50d6a0f307c5734bf6a4389cf56e8231a87e621
Marius Vintila <vintila.mariusm@gmail.com>
2020-11-08 21:47:54 +0200

    Added ludwig and header for preprocessed data
    Co-Authored-By: andreeac27 <69468694+andreeac27@users.noreply.github.com>
    Co-Authored-By: criscojocaru <40690927+criscojocaru@users.noreply.github.com>
    Co-Authored-By: paulbenge <63068592+paulbenge@users.noreply.github.com>
    Co-Authored-By: Ioana Raluca Petrescu <44580296+ralucapetrescu@users.noreply.github.com>

--- sources/main.py
+++ sources/main.py
@@ -1,18 +1,20 @@
 import sys
 import csv
+from ludwig.api import LudwigModel
 
 
 from preprocess import preprocess
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Incorrect number of arguments. Please use format:\npython main.py <path-to-input-csv-file> <path-to-output-csv-file>")
+    if len(sys.argv) != 4:
+        print("Incorrect number of arguments. Please use format:\npython main.py <path-to-input-csv-file> <path-to-output-csv-file> <ludwig-model-definition>")
 
     with open(sys.argv[1]) as f_in:
         c = csv.reader(f_in, delimiter=",")
 
-        processed_rows = []
+        processed_rows = [['source_institution', 'gender', 'age', 'declared_symptoms',
+                           'reported_symptoms', 'diagnosis', 'travel_history', 'contact_confirmation', 'test_result']]
         map_of_institution_codes = {}
         map_of_gender_codes = {}
 
@@ -33,3 +35,23 @@ if __name__ == "__main__":
             csv_writer = csv.writer(f_out)
             for row in processed_rows:
                 csv_writer.writerow(row)
+
+        # config = {
+        #     input_features:
+        #     [
+        #         {name: source_institution, type: category},
+        #         {name: gender, type: category},
+        #         {name: age, type: numerical},
+        #         {name: declared_symptoms, type: category},
+        #         {name: reported_symptoms, type: category},
+        #         {name: diagnosis, type: category},
+        #         {name: travel_history, type: category},
+        #         {name: contact_confirmation, type: category},
+        #         {name: test_result, type: category},
+        #     ],
+        #     output_features: [{name: test_result, type: category}],
+        # }
+        # model = LudwigModel(config)
+        # train_stats = model.train(processed_rows)
+
+        ludwig train --dataset sys.argv[2] --config sys.argv[3]
