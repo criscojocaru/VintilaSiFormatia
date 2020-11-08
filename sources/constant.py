2015c85ad60469c27c67e03016865a2449db0cff
Marius Vintila <vintila.mariusm@gmail.com>
2020-11-08 21:05:08 +0200

    Finished data preprocessing
    Finished data preprocessing for a row from the read csv file. For now, ignoring date fields and the public transit field
    
    Co-Authored-By: andreeac27 <69468694+andreeac27@users.noreply.github.com>
    Co-Authored-By: criscojocaru <40690927+criscojocaru@users.noreply.github.com>
    Co-Authored-By: paulbenge <63068592+paulbenge@users.noreply.github.com>
    Co-Authored-By: Ioana Raluca Petrescu <44580296+ralucapetrescu@users.noreply.github.com>

--- sources/constant.py
+++ sources/constant.py
@@ -1,2 +1,7 @@
 
-AGE_CONSTANTS = ["lun", "sap", "zi", "or", "nou"]
+AGE_CONSTANTS = ['lun', 'sap', 'zi', 'or', 'nou']
+SYMPTOMS_CONSTANTS = [' febr', 'afazie', 'amete', 'astenie', 'cefalee', 'corona', 'cov', 'covid', 'diare', 'disfagi', 'dispnee', 'dureri musculare',
+                      'fatigabilitate', 'friso', 'frisoane', 'gust', 'inapetenta', 'mialgi', 'miros', 'nazal', 'odinofagie', 'palpitatii', 'pneumo',
+                      'pozitiv', 'pulmo', 'respirat', 'sars', 'scaun', 'somno', 'temp', 'toracic', 'transpirati', 'tuse', 'vars', 'vertij']
+HISTORY_CONSTANTS = ['nu', 'mu', 'neag', 'ne', 'fara']
+CONTACT_CONSTANTS = ['da', 'focar', 'pozitiv', 'susp']
--- sources/preprocess.py
+++ sources/preprocess.py
@@ -1,3 +1,5 @@
+import datetime
+import calendar
 import re
 
 from constant import *
@@ -6,70 +8,130 @@ mapp = {}
 
 
 def get_highest_value_from_map(map):
-	if len(map):
-		return max(map.values())
-	return 0
+    if len(map):
+        return max(map.values())
+    return 0
 
 
 def encoding_as_map(string, map_of_codes):
-	if map_of_codes[string] == None:
-		map_of_codes[string] = get_highest_value_from_map(map_of_codes) + 1
-	return map_of_codes[string]
+    if map_of_codes.get(string) == None:
+        map_of_codes[string] = get_highest_value_from_map(map_of_codes) + 1
+    return map_of_codes.get(string)
 
 
 def source_institution(institution, map_of_institution_codes):
-	institution = institution.strip().upper()
-	return encoding_as_map(institution, map_of_institution_codes)
+    institution = institution.strip().upper()
+    return encoding_as_map(institution, map_of_institution_codes)
 
 
 def gender(gender, map_of_gender_codes):
-	gender = gender.strip().upper()
-	if gender == "F":
-		gender = "FEMININ"
-	elif gender == "M":
-		gender = "MASCULIN"
-	else:
-		gender = "ALTUL"
-	return encoding_as_map(gender, map_of_gender_codes)
+    gender = gender.strip().upper()
+    if gender == 'F':
+        gender = 'FEMININ'
+    elif gender == 'M':
+        gender = 'MASCULIN'
+    else:
+        gender = 'ALTUL'
+    return encoding_as_map(gender, map_of_gender_codes)
 
 
 def age(age):
-	age = age.upper()
-	if not age.isdigit():
-		if "an" in age:
-			match = re.findall(r'\d+ *an', age)
-			if len(match):
-				age_as_int = int(re.findall(r'\d+', match[0])[0])
-			else:
-				age_as_int = 0
-		elif any(x in age for x in AGE_CONSTANTS):
-			age_as_int = 0
-		else:
-			# default value for ages not matching any known format
-			age_as_int = 0
-	else:
-		age_as_int = int(age)
+    age = age.lower()
+    if not age.isdigit():
+        if 'an' in age:
+            match = re.findall(r'\d+ *an', age)
+            if len(match):
+                age_as_int = int(re.findall(r'\d+', match[0])[0])
+            else:
+                age_as_int = 0
+        elif any(x in age for x in AGE_CONSTANTS):
+            age_as_int = 0
+        else:
+            # default value for ages not matching any known format
+            age_as_int = 0
+    else:
+        age_as_int = int(age)
 
-	if age_as_int >= 0 and age_as_int <= 200:
-		return age_as_int
-	return 0
-
-
-def date():
-	return 0
+    if age_as_int >= 0 and age_as_int <= 200:
+        return age_as_int
+    return 0
 
 
-def symptoms():
-	return 0
+def date(date):
+    # Processing:
+    # YYYY-MM-dd
+    # Dd.MM.YYY
+    # Dd(./,)MM(./,)YYYY
+    year = 2020
+    month = 11
+    day = 8
+    # Encoding:
+    dt = datetime.datetime(year, month, day)
+    tt = datetime.datetime.timetuple(dt)
+    epoch = calendar.timegm(tt)
+    return epoch
 
 
-def travel_history():
-	return 0
+def symptoms(symptom):
+    symptom = symptom.lower()
+    if 'cov' in symptom and not 'asimpto' in symptom:
+        return 1
+    elif 'asimpto' in symptom:
+        return 0
+    elif any(x in symptom for x in SYMPTOMS_CONSTANTS):
+        return 1
+    elif len(symptom) == 0:
+        return -1
+    return 0
 
 
-def contact_confirmation():
-	return 0
+def travel_history(history):
+    history = history.strip().lower()
+    if any(x in history for x in HISTORY_CONSTANTS):
+        return 0
+    elif history == "0":
+        return 0
+    elif len(history) == 0:
+        return 0
+    return 1
 
 
-def test_result():
-	return 0
+def contact_confirmation(contact):
+    contact = contact.strip().lower()
+    if any(x in contact for x in CONTACT_CONSTANTS):
+        return 1
+    elif contact == "1":
+        return 1
+    return 0
+
+
+def test_result(result):
+    result = result.strip().upper()
+    if "POZITIV" in result:
+        return 1
+    return 0
+
+
+def ignore():
+    return None
+
+
+def preprocess(row, map_of_institution_codes, map_of_gender_codes):
+    preprocessed_row = []
+
+    preprocessed_row.append(source_institution(
+        row[0], map_of_institution_codes))
+    preprocessed_row.append(gender(row[1], map_of_gender_codes))
+    preprocessed_row.append(age(row[2]))
+    # preprocessed_row.append(date(row[3]))
+    preprocessed_row.append(symptoms(row[4]))
+    # preprocessed_row.append(date(row[5]))
+    preprocessed_row.append(symptoms(row[6]))
+    preprocessed_row.append(symptoms(row[7]))
+    preprocessed_row.append(travel_history(row[8]))
+    # preprocessed_row.append(ignore())
+    preprocessed_row.append(contact_confirmation(row[10]))
+    # preprocessed_row.append(date(row[11]))
+    preprocessed_row.append(test_result(row[12]))
+
+    return preprocessed_row
