14d5d5055067f6d02e3ed0aeca879f1d1ea08296
Marius Vintila <vintila.mariusm@gmail.com>
2020-11-14 22:11:36 +0200

    Sources modifications
    - Modified experiment to predict
    - Fixed bug on generatic csv with an empty line between rows
    - Not preprocessing last column when we're trying to predict
    - Modifying max epochs to 10
    
    Co-Authored-By: andreeac27 <69468694+andreeac27@users.noreply.github.com>
    Co-Authored-By: paulbenge <63068592+paulbenge@users.noreply.github.com>
    Co-Authored-By: criscojocaru <40690927+criscojocaru@users.noreply.github.com>
    Co-Authored-By: Ioana Raluca Petrescu <44580296+ralucapetrescu@users.noreply.github.com>

--- sources/main.py
+++ sources/main.py
@@ -7,7 +7,7 @@ from ludwig.api import LudwigModel
 from preprocess import preprocess
 
 
-def preprocess_dataset(input_file, output_file):
+def preprocess_dataset(input_file, output_file, process_type):
     with open(input_file) as f_in:
         c = csv.reader(f_in, delimiter=",")
 
@@ -23,13 +23,13 @@ def preprocess_dataset(input_file, output_file):
             elif i != 0:
                 # print(row)
                 processed_row = preprocess(
-                    row, map_of_institution_codes, map_of_gender_codes)
+                    row, map_of_institution_codes, map_of_gender_codes, process_type)
                 processed_rows.append(processed_row)
                 i += 1
             elif i == 0:
                 i += 1
 
-        with open(output_file, 'w+') as f_out:
+        with open(output_file, 'w+', newline='') as f_out:
             csv_writer = csv.writer(f_out)
             for row in processed_rows:
                 csv_writer.writerow(row)
@@ -40,22 +40,22 @@ if __name__ == "__main__":
         if len(sys.argv) != 5:
             print("Incorrect number of arguments. Please use format:\npython main.py train <path-to-input-csv-file> <path-to-output-csv-file> <ludwig-model-definition>")
 
-        preprocess_dataset(sys.argv[2], sys.argv[3])
+        preprocess_dataset(sys.argv[2], sys.argv[3], 1)
 
         config = sys.argv[4]
         model = LudwigModel(config)
-        train_stats = model.train(dataset=sys.argv[3], experiment_name='covid_experiment')
+        train_stats = model.train(dataset=sys.argv[3], experiment_name='covid_inference', model_name='train')
 
         print(train_stats)
     elif sys.argv[1] == "experiment":
         if len(sys.argv) != 6:
-            print("Incorrect number of arguments. Please use format:\npython main.py experiment <path-to-trained-model> <path-to-input-csv-file> <path-to-output-csv-file> <ludwig-model-definition>")
+            print("Incorrect number of arguments. Please use format:\npython main.py predict <path-to-trained-model> <path-to-input-csv-file> <path-to-output-csv-file> <ludwig-model-definition>")
 
-        preprocess_dataset(sys.argv[3], sys.argv[4])
+        preprocess_dataset(sys.argv[3], sys.argv[4], 0)
 
         config = sys.argv[5]
         model = LudwigModel.load(sys.argv[2])
-        train_stats = model.experiment(sys.argv[4], experiment_name='covid_experiment')
+        train_stats = model.predict(dataset=sys.argv[4])
         print(train_stats)
     else:
         print(
--- sources/preprocess.py
+++ sources/preprocess.py
@@ -116,7 +116,7 @@ def ignore():
     return None
 
 
-def preprocess(row, map_of_institution_codes, map_of_gender_codes):
+def preprocess(row, map_of_institution_codes, map_of_gender_codes, process_type):
     preprocessed_row = []
 
     preprocessed_row.append(source_institution(
@@ -132,6 +132,7 @@ def preprocess(row, map_of_institution_codes, map_of_gender_codes):
     # preprocessed_row.append(ignore())
     preprocessed_row.append(contact_confirmation(row[10]))
     # preprocessed_row.append(date(row[11]))
-    preprocessed_row.append(test_result(row[12]))
+    if (process_type == 1):
+        preprocessed_row.append(test_result(row[12]))
 
     return preprocessed_row
