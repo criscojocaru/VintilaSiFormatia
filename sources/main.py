4bf186d506344e64711bad50ef2b0c1151a49598
Marius Vintila <vintila.mariusm@gmail.com>
2020-11-21 18:12:40 +0200

    Changed epochs; Filtering results

--- sources/main.py
+++ sources/main.py
@@ -24,7 +24,9 @@ def preprocess_dataset(input_file, output_file, process_type):
                 # print(row)
                 processed_row = preprocess(
                     row, map_of_institution_codes, map_of_gender_codes, process_type)
-                processed_rows.append(processed_row)
+                if (processed_row[3] != -1 and processed_row[4] != -1 and processed_row[5] != -1):
+                    processed_rows.append(processed_row)
+
                 i += 1
             elif i == 0:
                 i += 1
@@ -51,11 +53,13 @@ if __name__ == "__main__":
         if len(sys.argv) != 6:
             print("Incorrect number of arguments. Please use format:\npython main.py predict <path-to-trained-model> <path-to-input-csv-file> <path-to-output-csv-file> <ludwig-model-definition>")
 
-        preprocess_dataset(sys.argv[3], sys.argv[4], 0)
+        preprocess_dataset(sys.argv[3], sys.argv[4], 1)
 
         config = sys.argv[5]
         model = LudwigModel.load(sys.argv[2])
-        train_stats = model.predict(dataset=sys.argv[4])
+        # train_stats = model.predict(dataset=sys.argv[4])
+        train_stats = model.experiment(training_set=sys.argv[4], validation_set=sys.argv[4], test_set=sys.argv[4])
+        # train_stats = model.evaluate(dataset=sys.argv[4], skip_save_predictions=False, skip_save_eval_stats=False, collect_predictions=True, collect_overall_stats=True)
         print(train_stats)
     else:
         print(
